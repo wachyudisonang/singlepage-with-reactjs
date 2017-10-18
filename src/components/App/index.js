cda61f7eb4c50814a186095043ad7fc4c398ab49
{sonangsatriani} <wachyudi.sonang@gmail.com>
2017-10-18 19:25:23 +0700

    update landing page (App/index.js), new page: About & NotFound

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+// src/components/App/index.js
+import React, { PropTypes, Component } from 'react';
+import classnames from 'classnames';
+
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
   render() {
+    const { className, ...props } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className={classnames('App', className)} {...props}>
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
