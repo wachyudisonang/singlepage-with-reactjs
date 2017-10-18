c5d7ea630c2dc9ed8619e0fea5da7f90e61c115c
{sonangsatriani} <wachyudi.sonang@gmail.com>
2017-10-18 20:19:30 +0700

    working page routing: add pages

--- src/components/App.js
+++ src/components/App.js
@@ -4,26 +4,26 @@ import classnames from 'classnames';
 
 import logo from './logo.svg';
 import './style.css';
+import Header from './Header'
+import Main from './Main'
 
 class App extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
+	// static propTypes = {}
+	// static defaultProps = {}
+	// state = {}
 
-  render() {
-    const { className, ...props } = this.props;
-    return (
-      <div className={classnames('App', className)} {...props}>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		const { className, ...props } = this.props;
+		return (
+			<div className={classnames('App', className)} {...props}>
+				<img src={logo} className="App-logo" alt="logo" />
+				<div>
+					<Header />
+					<Main />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
