c7a98019b4fac213ea708b6755f92233ae884cfe
Wachyudi Sonang <wachyudi.sonang@gmail.com>
2017-10-26 03:57:05 +0700

    fix modal get state

--- src/components/buttonModal.js
+++ src/components/buttonModal.js
@@ -11,26 +11,21 @@ class buttonModal extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			modal: false
+			modal: false,
+			projectName: "",
+			projectSummary: ""
 		};
 
 		this.toggle = this.toggle.bind(this);
 	}
 
-	toggle() {
+	toggle(projectName, projectSummary) {
 		this.setState({
+			projectName: projectName,
+			projectSummary: projectSummary,
 			modal: !this.state.modal
 		});
 	}
-
-	toggleSetContent(modalTitle) {
-		this.toggle = this.toggle.bind(this);
-		this.setState({
-			modal: !this.state.modal
-		});
-	}
-
-
 
 	render() {
 
@@ -39,9 +34,15 @@ class buttonModal extends React.Component {
 			r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
 			return images;
 		}
+		
+		function getVal(obj) {
+			if (obj != null && obj != '') {
+				return JSON.stringify(obj).replace(/\"/g, "");
+			}
+		}
+
 		return (
 			<Row>
-
 				{this.props.sections
 					.filter(function (project, index) {
 						return !project.hide
@@ -50,8 +51,8 @@ class buttonModal extends React.Component {
 					const images = importAll(require.context('../resources/img', false, /\.(png|jpe?g|svg)$/));
 					return (
 						<div class="col-sm-4 portfolio-item" key={index}>
-							<a class="portfolio-link" onClick={this.toggle} data-toggle="modal">
-								<div class="caption" onClick={this.toggle}>
+							<a class="portfolio-link" onClick={this.toggle.bind(this, project.name, project.summary)} data-toggle="modal">
+								<div class="caption">
 									<div class="caption-content">
 										<i class="icon-search-plus display-4"></i>
 										<h2 hidden>{project.name}</h2>
@@ -63,15 +64,13 @@ class buttonModal extends React.Component {
 						</div>
 					);
 				}, this)}
-
-								<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-									<ModalHeader toggle={this.toggle}>788888</ModalHeader>
-									<ModalBody>788888</ModalBody>
-									<ModalFooter>
-										<Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-										<Button color="secondary" onClick={this.toggle}>Cancel</Button>
-									</ModalFooter>
-								</Modal>
+				<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+					<ModalHeader toggle={this.toggle}>{getVal(this.state.projectName)}</ModalHeader>
+					<ModalBody>{getVal(this.state.projectSummary)}</ModalBody>
+					<ModalFooter>
+						<Button color="primary" onClick={this.toggle}>Close</Button>
+					</ModalFooter>
+				</Modal>
 			</Row>
 		);
 	}
